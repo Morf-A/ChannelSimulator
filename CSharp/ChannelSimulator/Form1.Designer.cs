8ac3d1a1d07178c2a6423a1cf4c65d81321c2f0d
Morf-A <Morf-a@yandex.ru>
2014-05-19 02:15:07 +0300

    Улучшил внешний вид приложения

--- CSharp/ChannelSimulator/Form1.Designer.cs
+++ CSharp/ChannelSimulator/Form1.Designer.cs
@@ -28,6 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label2 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
@@ -48,15 +52,20 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 18);
+            this.label2.Location = new System.Drawing.Point(8, 18);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(92, 13);
             this.label2.TabIndex = 1;
@@ -69,18 +78,19 @@
             "Extended Pedestrian A model (EPA)",
             "Extended Vehicular A model (EVA)",
             "Extended Typical Urban model (ETU)"});
-            this.comboBox1.Location = new System.Drawing.Point(120, 18);
+            this.comboBox1.Location = new System.Drawing.Point(110, 15);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(222, 21);
+            this.comboBox1.Size = new System.Drawing.Size(218, 21);
             this.comboBox1.TabIndex = 2;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(242, 293);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(1, 280);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(135, 23);
+            this.button1.Size = new System.Drawing.Size(327, 27);
             this.button1.TabIndex = 3;
-            this.button1.Text = "Создать канал";
+            this.button1.Text = "Отобразить канал";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -88,21 +98,23 @@
             // 
             this.radioButtonMEDS.AutoSize = true;
             this.radioButtonMEDS.Checked = true;
-            this.radioButtonMEDS.Location = new System.Drawing.Point(22, 28);
+            this.radioButtonMEDS.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButtonMEDS.Location = new System.Drawing.Point(19, 28);
             this.radioButtonMEDS.Name = "radioButtonMEDS";
-            this.radioButtonMEDS.Size = new System.Drawing.Size(56, 17);
+            this.radioButtonMEDS.Size = new System.Drawing.Size(124, 22);
             this.radioButtonMEDS.TabIndex = 5;
             this.radioButtonMEDS.TabStop = true;
-            this.radioButtonMEDS.Text = "MEDS";
+            this.radioButtonMEDS.Text = "MEDS Method";
             this.radioButtonMEDS.UseVisualStyleBackColor = true;
             this.radioButtonMEDS.CheckedChanged += new System.EventHandler(this.radioButtonMEDS_CheckedChanged);
             // 
             // radioButtonJM
             // 
             this.radioButtonJM.AutoSize = true;
-            this.radioButtonJM.Location = new System.Drawing.Point(206, 28);
+            this.radioButtonJM.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButtonJM.Location = new System.Drawing.Point(192, 28);
             this.radioButtonJM.Name = "radioButtonJM";
-            this.radioButtonJM.Size = new System.Drawing.Size(89, 17);
+            this.radioButtonJM.Size = new System.Drawing.Size(116, 22);
             this.radioButtonJM.TabIndex = 6;
             this.radioButtonJM.Text = "JakesMethod";
             this.radioButtonJM.UseVisualStyleBackColor = true;
@@ -111,7 +123,8 @@
             // 
             this.groupBox1.Controls.Add(this.radioButtonMEDS);
             this.groupBox1.Controls.Add(this.radioButtonJM);
-            this.groupBox1.Location = new System.Drawing.Point(15, 45);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(1, 54);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(327, 64);
             this.groupBox1.TabIndex = 8;
@@ -121,7 +134,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(22, 134);
+            this.label1.Location = new System.Drawing.Point(8, 137);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(266, 13);
             this.label1.TabIndex = 9;
@@ -130,7 +143,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(22, 194);
+            this.label3.Location = new System.Drawing.Point(8, 197);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(167, 13);
             this.label3.TabIndex = 10;
@@ -139,7 +152,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(22, 165);
+            this.label4.Location = new System.Drawing.Point(8, 167);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(230, 13);
             this.label4.TabIndex = 13;
@@ -147,7 +160,7 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(307, 134);
+            this.textBox3.Location = new System.Drawing.Point(280, 134);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(48, 20);
             this.textBox3.TabIndex = 12;
@@ -157,7 +170,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(22, 225);
+            this.label5.Location = new System.Drawing.Point(8, 228);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(215, 13);
             this.label5.TabIndex = 16;
@@ -166,7 +179,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(22, 255);
+            this.label6.Location = new System.Drawing.Point(8, 258);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(129, 13);
             this.label6.TabIndex = 17;
@@ -174,7 +187,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(307, 225);
+            this.textBox1.Location = new System.Drawing.Point(280, 225);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(48, 20);
             this.textBox1.TabIndex = 18;
@@ -182,7 +195,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(307, 255);
+            this.textBox2.Location = new System.Drawing.Point(280, 251);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(48, 20);
             this.textBox2.TabIndex = 19;
@@ -190,7 +203,7 @@
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(307, 194);
+            this.textBox4.Location = new System.Drawing.Point(280, 194);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(48, 20);
             this.textBox4.TabIndex = 20;
@@ -198,7 +211,7 @@
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(307, 165);
+            this.textBox5.Location = new System.Drawing.Point(280, 160);
             this.textBox5.Name = "textBox5";
             this.textBox5.ReadOnly = true;
             this.textBox5.Size = new System.Drawing.Size(48, 20);
@@ -206,63 +219,125 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(177, 420);
+            this.button2.Location = new System.Drawing.Point(1, 411);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(200, 23);
+            this.button2.Size = new System.Drawing.Size(212, 32);
             this.button2.TabIndex = 22;
             this.button2.Text = "Пропустить сигнал через канал";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(206, 322);
+            this.button3.Location = new System.Drawing.Point(1, 313);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(171, 23);
+            this.button3.Size = new System.Drawing.Size(207, 23);
             this.button3.TabIndex = 23;
             this.button3.Text = "Построить автокорреляцию";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(206, 351);
+            this.button4.Location = new System.Drawing.Point(1, 342);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(171, 23);
+            this.button4.Size = new System.Drawing.Size(207, 23);
             this.button4.TabIndex = 24;
-            this.button4.Text = "Построить PDS Джейска";
+            this.button4.Text = "Построить СПМ Джейска";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(177, 391);
+            this.button5.Location = new System.Drawing.Point(1, 371);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(200, 23);
+            this.button5.Size = new System.Drawing.Size(212, 32);
             this.button5.TabIndex = 25;
-            this.button5.Text = "Построить FFT";
+            this.button5.Text = "Построить частотный отклик канала";
             this.button5.UseVisualStyleBackColor = true;
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(347, 12);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(696, 431);
+            this.chart1.TabIndex = 26;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button6.Image = ((System.Drawing.Image)(resources.GetObject("button6.Image")));
+            this.button6.Location = new System.Drawing.Point(219, 371);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(109, 32);
+            this.button6.TabIndex = 31;
+            this.button6.Text = "Сохранить";
+            this.button6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button6.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button7.Image = ((System.Drawing.Image)(resources.GetObject("button7.Image")));
+            this.button7.Location = new System.Drawing.Point(219, 411);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(109, 32);
+            this.button7.TabIndex = 32;
+            this.button7.Text = "Сохранить";
+            this.button7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button7.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button8.Image = ((System.Drawing.Image)(resources.GetObject("button8.Image")));
+            this.button8.Location = new System.Drawing.Point(219, 309);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(109, 56);
+            this.button8.TabIndex = 33;
+            this.button8.Text = "Очистить данные";
+            this.button8.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button8.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.button8.UseVisualStyleBackColor = true;
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(435, 57);
+            this.richTextBox1.Location = new System.Drawing.Point(452, 12);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(118, 386);
-            this.richTextBox1.TabIndex = 26;
+            this.richTextBox1.Size = new System.Drawing.Size(126, 431);
+            this.richTextBox1.TabIndex = 34;
             this.richTextBox1.Text = "";
             // 
             // richTextBox2
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(589, 57);
+            this.richTextBox2.Location = new System.Drawing.Point(698, 12);
             this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(118, 386);
-            this.richTextBox2.TabIndex = 27;
+            this.richTextBox2.Size = new System.Drawing.Size(110, 431);
+            this.richTextBox2.TabIndex = 35;
             this.richTextBox2.Text = "";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(752, 475);
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1054, 449);
             this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.chart1);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
@@ -281,10 +356,14 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label2);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Симутятор канала";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -312,6 +391,10 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.RichTextBox richTextBox2;
 
