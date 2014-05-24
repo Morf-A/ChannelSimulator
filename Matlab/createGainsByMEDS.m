ebdb1849cef59234b208006bcc64d70cf268fcf9
Morf-A <Morf-a@yandex.ru>
2014-05-24 16:53:43 +0400

    Подправил получение gains для MEDS, чтобы выход был как в JM

--- Matlab/createGainsByMEDS.m
+++ Matlab/createGainsByMEDS.m
@@ -1,9 +1,11 @@
 function c = createGainsByMEDS(profile, sinusoidsNumber)
     rays = length(profile);
-    c = zeros(2,rays);
+    c = zeros(2,sinusoidsNumber+1,rays);
     for i=1:2
         for z=1:rays
-            c(i,z) = profile(z)*sqrt(2/sinusoidsNumber);
+            for n = 1: sinusoidsNumber+1
+                c(i,n,z) = profile(z)*sqrt(2/sinusoidsNumber);
+            end
         end
         sinusoidsNumber=sinusoidsNumber+1;
     end
